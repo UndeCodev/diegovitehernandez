a59f2022fab63e8eccda0590e2f937b23740c16c
Diego Vite Hernández <undecode0@hotmail.com>
2024-10-04 21:24:04 -0600

    Update 26

---
+++ assets/InfoPage-B7HePr2A.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as n,a as r,o as t}from"./index-UHJuMDJv.js";const a={};function s(c,e){return t(),n("div",null,e[0]||(e[0]=[r("h1",null,"Info",-1)]))}const _=o(a,[["render",s]]);export{_ as default};
